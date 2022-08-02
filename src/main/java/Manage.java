3b23559edbc476471cce1931220b9af8b2b3beb3
eliyaAharon <eliyahron13@gmail.com>
2022-08-02 19:46:37 +0300

    graphics completed

--- src/main/java/Manage.java
+++ src/main/java/Manage.java
@@ -3,17 +3,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.Window;
 import java.awt.image.BufferedImage;
 import java.net.URL;
 
 public class Manage extends JPanel {
     // fields
     private static ChromeDriver driver;
-    private static int x = 350, y = 100, width = 200, height = 50;
-    private static BufferedImage bufferedImage;
-    private static BufferedImage b;
-    private static BufferedImage b1;
+    private static int x = 395, y = 20, width = 175, height = 50;
+    private static BufferedImage afterFilter;
+    private static BufferedImage firstBuffer;
+    private static BufferedImage originalImage;
+    private static final int yOfButtons = 90;
+    private static JButton filter1, filter2, filter3, filter4, filter5, filter6;
 
 
     // My constructor
@@ -31,7 +32,7 @@ public class Manage extends JPanel {
         this.add(name);
         frame.setVisible(true);
 
-        // button setting
+        // Main button setting
         Font buttonFont = new Font("Arial", Font.BOLD, 17);
         JButton nameButton = new JButton("search ");
         nameButton.setFont(buttonFont);
@@ -39,41 +40,81 @@ public class Manage extends JPanel {
         nameButton.setBounds(this.x + this.width, this.y, this.width - 80, this.height);
         this.add(nameButton);
 
-        // button affect
+        // Main button affect
         nameButton.addActionListener((e -> {
             try {
                 String s = "https://dalicanvas.co.il/wp-content/uploads/2020/02/%D7%A9%D7%A7%D7%99%D7%A2%D7%94-%D7%A7%D7%9C%D7%90%D7%A1%D7%99%D7%AA-1.jpg";
                 URL url = new URL(s);
-
-                b = ImageIO.read(url);
-                bufferedImage = resize(b, new Dimension(350, 500));
-                b1 = resize(b, new Dimension(350, 500));
-                JLabel l = new JLabel(new ImageIcon(b1));
+                // set the buffers
+                firstBuffer = ImageIO.read(url);
+                afterFilter = resize(firstBuffer, new Dimension(350, 500));
+                originalImage = resize(firstBuffer, new Dimension(350, 500));
+                JLabel l = new JLabel(new ImageIcon(originalImage));
                 System.out.println("i");
                 l.setBounds(40, 50, 350, 500);
                 this.add(l);
+                // set the first filter button
+                filter1 = new JButton();
+                createCustomButton(filter1, this.yOfButtons, "first filter");
+                this.add(filter1);
+
+                // set the second filter button
+                filter2 = new JButton();
+                createCustomButton(filter2, this.yOfButtons + 90, "second filter");
+                this.add(filter2);
+
+                // set the third filter button
+                filter3 = new JButton();
+                createCustomButton(filter3, filter2.getY() + 90, "third filter");
+                this.add(filter3);
+
+                // set the fourth filter button
+                filter4 = new JButton();
+                createCustomButton(filter4, filter3.getY() + 90, "forth filter");
+                this.add(filter4);
+
+                // set the fifth filter button
+                filter5 = new JButton();
+                createCustomButton(filter5, filter4.getY() + 90, "fifth filter");
+                this.add(filter5);
+
+                // set the sixth filter button
+                filter6 = new JButton();
+                createCustomButton(filter6, filter5.getY() + 90, " sixth  filter");
+                this.add(filter6);
                 paint(getGraphics());
-//                Thread.sleep(3000);
-//                blue(bufferedImage);
-//                paint(getGraphics());
-//                Thread.sleep(3000);
-//                red(bufferedImage);
-//                paint(getGraphics());
+
+                // buttons affects (all of them )
+                filter1.addActionListener((e1 -> {
+                    red(afterFilter);
+                    paint(getGraphics());
+                }));
+
+                filter2.addActionListener((e1 -> {
+
+                }));
+
+                filter3.addActionListener((e1 -> {
+
+                }));
+
+                filter4.addActionListener((e1 -> {
+
+                }));
+
+                filter5.addActionListener((e1 -> {
+
+                }));
+
+                filter6.addActionListener((e1 -> {
+
+                }));
+
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
         }));
 
-
-    }
-
-    public void blue(BufferedImage b) {
-        for (int x = 0; x < b.getWidth(); x++) {
-            for (int y = 0; y < b.getHeight(); y++) {
-                int z = Color.green.getRGB();
-                b.setRGB(x, y, z);
-            }
-        }
     }
 
     public void red(BufferedImage b) {
@@ -85,11 +126,13 @@ public class Manage extends JPanel {
         }
     }
 
+    // paint method
     public void paint(Graphics g) {
         super.paint(g);
-        g.drawImage(bufferedImage, 700, 50, null);
+        g.drawImage(afterFilter, 700, 50, null);
     }
 
+    // Resize the bufferImage
     public BufferedImage resize(BufferedImage image, final Dimension size) {
         final BufferedImage resized = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_ARGB);
         final Graphics2D g = resized.createGraphics();
@@ -98,5 +141,15 @@ public class Manage extends JPanel {
         return resized;
     }
 
+    // create custom button
+    public static void createCustomButton(JButton button, int y, String tittle) {
+        Font buttonFont = new Font("Arial", Font.BOLD, 17);
+        button.setBackground(Color.YELLOW);
+        button.setBounds(460, y, 175, 50);
+        button.setText(tittle);
+        button.setFont(buttonFont);
+        }
+    }
 
-}
+
+
