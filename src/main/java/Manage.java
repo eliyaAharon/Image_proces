762cee160ce27538435bd99e1303068b3088a46d
eliyaAharon <eliyahron13@gmail.com>
2022-08-03 10:55:20 +0300

    fix switch case

--- src/main/java/Manage.java
+++ src/main/java/Manage.java
@@ -18,7 +18,6 @@ public class Manage extends JPanel {
     private static final int yOfButtons = 90;
     private static JButton filter1, filter2, filter3, filter4, filter5, filter6;
 
-
     // My constructor
     public Manage(int x, int y, int width, int height, JFrame frame) {
         this.setBounds(x, y, width, height);
@@ -54,7 +53,6 @@ public class Manage extends JPanel {
                 afterFilter = resize(firstBuffer, new Dimension(350, 500));
                 originalImage = resize(firstBuffer, new Dimension(350, 500));
                 JLabel l = new JLabel(new ImageIcon(originalImage));
-                System.out.println("i");
                 l.setBounds(40, 50, 350, 500);
                 this.add(l);
                 // set the first filter button
@@ -91,7 +89,7 @@ public class Manage extends JPanel {
                 // buttons effects (all of them )
                 filter1.addActionListener((e1 -> {
                     afterFilter = resize(firstBuffer, new Dimension(350, 500));
-                  //  red(afterFilter);
+                    //  red(afterFilter);
                     colorsChange(afterFilter, 6);
                     paint(getGraphics());
                 }));
@@ -133,6 +131,7 @@ public class Manage extends JPanel {
         }));
 
     }
+
     // paint method
     public void paint(Graphics g) {
         super.paint(g);
@@ -163,35 +162,29 @@ public class Manage extends JPanel {
         for (int x = 0; x < b.getWidth(); x++) {
             for (int y = 0; y < b.getHeight(); y++) {
                 int pixel = b.getRGB(x, y);
-
+                Color color = new Color(pixel);
                 switch (n) {
                     case 1:
-                        Color color = new Color(pixel);
                         Color newColor = blackAndWhite(color);
                         b.setRGB(x, y, newColor.getRGB());
                         break;
                     case 2:
-                        Color color2 = new Color(pixel);
-                        Color newColor2 = reverseImgColor(color2);
+                        Color newColor2 = reverseImgColor(color);
                         b.setRGB(x, y, newColor2.getRGB());
                         break;
-                    case 3 :
-                        Color color3 = new Color(pixel);
-                        afterFilter.setRGB(b.getWidth() -x -1 , y,color3.getRGB());
+                    case 3:
+                        b.setRGB(b.getWidth() - x - 1, y, color.getRGB());
                         break;
-                    case 4 :
-                        Color color4 = new Color(pixel);
-                        Color newColor4 = warm(color4);
+                    case 4:
+                        Color newColor4 = warm(color);
                         b.setRGB(x, y, newColor4.getRGB());
                         break;
                     case 5:
-                        Color color5 = new Color(pixel);
-                        Color newColor5 = brighter(color5);
+                        Color newColor5 = brighter(color);
                         b.setRGB(x, y, newColor5.getRGB());
                         break;
                     case 6:
-                        Color color6 = new Color(pixel);
-                        Color newColor6 = colorShiftLeft(color6);
+                        Color newColor6 = colorShiftLeft(color);
                         b.setRGB(x, y, newColor6.getRGB());
                         break;
 
@@ -229,52 +222,56 @@ public class Manage extends JPanel {
         Color newColor = new Color(red, green, blue);
         return newColor;
     }
-    public static Color warm (Color color) {
+
+    public static Color warm(Color color) {
         int red = color.getRed();
         int green = color.getGreen();
         int blue = color.getBlue();
 
-        int outputRed = (int)((red * 0.393) + (green *0.769) + (blue * 0.189));
-        int outputGreen = (int)((red * 0.349) + (green *0.686) + (blue * 0.168));
-        int outputBlue = (int)((red * 0.272) + (green *0.534) + (blue * 0.131));
+        int outputRed = (int) ((red * 0.393) + (green * 0.769) + (blue * 0.189));
+        int outputGreen = (int) ((red * 0.349) + (green * 0.686) + (blue * 0.168));
+        int outputBlue = (int) ((red * 0.272) + (green * 0.534) + (blue * 0.131));
 
-        Color warmFilter = new Color(topColor(outputRed,1),topColor(outputGreen,1),topColor(outputBlue,1));
+        Color warmFilter = new Color(topColor(outputRed, 1), topColor(outputGreen, 1), topColor(outputBlue, 1));
         return warmFilter;
     }
-    public static int topColor (int original, double timesBy) {
+
+    public static int topColor(int original, double timesBy) {
         original *= timesBy;
         if (original > 255) {
             original = 255;
         }
         return original;
     }
-    public static Color brighter (Color color) {
+
+    public static Color brighter(Color color) {
 
         int red = color.getRed();
-        if (red < 210){
-            red = red+45;
+        if (red < 210) {
+            red = red + 45;
         }
 
         int green = color.getGreen();
-        if (green < 210){
-            green = green+45;
+        if (green < 210) {
+            green = green + 45;
         }
 
         int blue = color.getBlue();
-        if (blue < 210){
-            blue = blue+45;
+        if (blue < 210) {
+            blue = blue + 45;
         }
 
         Color newColor = new Color(red, green, blue);
         return newColor;
     }
-    public static Color colorShiftLeft (Color color) {
+
+    public static Color colorShiftLeft(Color color) {
 
         int red = color.getRed();
         int green = color.getGreen();
         int blue = color.getBlue();
 
-        Color newColor = new Color(blue,red, green);
+        Color newColor = new Color(blue, red, green);
         return newColor;
     }
 }
